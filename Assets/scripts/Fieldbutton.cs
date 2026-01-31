9115090881f92f1dc65738562b0b653c8948b2c3
MaximDMD25 <DMD25.MBanasinski@stud.fh-dresden.eu>
2026-01-31 17:19:29 +0100

    feat: clean up

--- Assets/scripts/Fieldbutton.cs
+++ Assets/scripts/Fieldbutton.cs
@@ -10,23 +10,20 @@ using Unity.VisualScripting;// Required when using Event data.
 public class FieldButton : MonoBehaviour, IPointerEnterHandler
 {
     private TicTacToeManager ticTacToeManager;
-    private bool hasbeenclicked = false;
-    private Button button;
+    private bool hasBeenClicked = false;
     private AudioSource audioSource;
     public AudioClip hoverClip;
     public AudioClip pressedClip;
     public AudioClip disabledClip;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         ticTacToeManager = FindFirstObjectByType<TicTacToeManager>();
-        button = GetComponent<Button>();
         audioSource = GetComponent<AudioSource>();
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (hasbeenclicked)
+        if (hasBeenClicked)
             return;
         audioSource.PlayOneShot(hoverClip, 0.6f);
         
@@ -34,7 +31,7 @@ public class FieldButton : MonoBehaviour, IPointerEnterHandler
 
     public void OnButtonClicked()
     {
-        if (hasbeenclicked)
+        if (hasBeenClicked)
         {
             audioSource.PlayOneShot(disabledClip, 1f);
             return;
@@ -44,29 +41,11 @@ public class FieldButton : MonoBehaviour, IPointerEnterHandler
         ticTacToeManager.OnButtonClickedInManager(this);
         transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 1, 2, 1);
         transform.DOPunchRotation(new Vector3(15, 15,15), 1, 5, 2);
-        hasbeenclicked = true;
-        //button.enabled = false;
+        hasBeenClicked = true;
     }
 
     public void SetField(int currentPlayer)
     {
         GetComponentInChildren<TextMeshProUGUI>().text = currentPlayer == 0 ? "X" : "O";
-
-        // Die obere Zeile verwendet den sog. ternary operator und erwirkt
-        // exakt das gleiche Ergebnis wie die unteren 13 Zeilen, nur kürzer
-
-        //TextMeshProUGUI textField = GetComponentInChildren<TextMeshProUGUI>();
-
-        //string symbol;
-        //if(currentPlayer == 0)
-        //{
-        //    symbol = "X";
-        //}
-        //else
-        //{
-        //    symbol ="O";
-        //}
-
-        //textField.text = symbol;
     }
 }
--- Assets/scripts/tictactoemanager.cs
+++ Assets/scripts/tictactoemanager.cs
@@ -10,21 +10,19 @@ public class TicTacToeManager : MonoBehaviour
     int currentPlayer = 0;
     int clicks = 0;
     bool gameended = false;
-    public TextMeshProUGUI playertext;
+    public TextMeshProUGUI playerText;
     public TextMeshProUGUI[] fieldButtons;
     private AudioSource audioSource;
     public AudioClip winClip;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playertext.text = "Spieler X";
+        playerText.text = "Spieler X";
         audioSource = GetComponent<AudioSource>();
     }
 
     public void OnButtonClickedInManager(FieldButton fieldButton)
     {
-        //Debug.Log("JUHU ICH WURDE IM MANAGER GEKLICKT!!!11");
         if (gameended)
             return;
         clicks++;
@@ -36,75 +34,74 @@ public class TicTacToeManager : MonoBehaviour
 
         if (currentPlayer == 0)
 
-            playertext.text = "Spieler X";
+            playerText.text = "Spieler X";
 
         else
-            playertext.text = "Spieler 0";
+            playerText.text = "Spieler 0";
 
-        checkwinningconditions();
+        CheckWinningConditions();
 
     }
 
-    public void checkwinningconditions()
+    public void CheckWinningConditions()
     {
 
-        //if (fieldButtons[0].text == fieldButtons[1].text && fieldButtons[0].text == fieldButtons[3].text)
         if (fieldButtons[0].text != string.Empty && fieldButtons[0].text.Equals(fieldButtons[1].text) && fieldButtons[0].text.Equals(fieldButtons[2].text))
         {
-            winner();
-            winnerButtons(fieldButtons[0], fieldButtons[1], fieldButtons[2]);
+            Winner();
+            WinnerButtons(fieldButtons[0], fieldButtons[1], fieldButtons[2]);
         }
 
         else if(fieldButtons[3].text != string.Empty && fieldButtons[3].text.Equals(fieldButtons[4].text) && fieldButtons[3].text.Equals(fieldButtons[5].text))
         {
-            winner();
-            winnerButtons(fieldButtons[3], fieldButtons[4], fieldButtons[5]);
+            Winner();
+            WinnerButtons(fieldButtons[3], fieldButtons[4], fieldButtons[5]);
         }
         else if(fieldButtons[6].text != string.Empty && fieldButtons[6].text.Equals(fieldButtons[7].text) && fieldButtons[6].text.Equals(fieldButtons[8].text))
         {
-            winner();
-            winnerButtons(fieldButtons[6], fieldButtons[7], fieldButtons[8]);
+            Winner();
+            WinnerButtons(fieldButtons[6], fieldButtons[7], fieldButtons[8]);
         }
         else if(fieldButtons[0].text != string.Empty && fieldButtons[0].text.Equals(fieldButtons[4].text) && fieldButtons[0].text.Equals(fieldButtons[8].text))
         {
-            winner();
-            winnerButtons(fieldButtons[0], fieldButtons[4], fieldButtons[8]);
+            Winner();
+            WinnerButtons(fieldButtons[0], fieldButtons[4], fieldButtons[8]);
         }
         else if(fieldButtons[2].text != string.Empty && fieldButtons[2].text.Equals(fieldButtons[4].text) && fieldButtons[2].text.Equals(fieldButtons[6].text))
         {
-            winner();
-            winnerButtons(fieldButtons[2], fieldButtons[4], fieldButtons[6]);
+            Winner();
+            WinnerButtons(fieldButtons[2], fieldButtons[4], fieldButtons[6]);
         }
         else if(fieldButtons[0].text != string.Empty && fieldButtons[0].text.Equals(fieldButtons[1].text) && fieldButtons[0].text.Equals(fieldButtons[2].text))
         {
-            winner();
-            winnerButtons(fieldButtons[0], fieldButtons[1], fieldButtons[2]);
+            Winner();
+            WinnerButtons(fieldButtons[0], fieldButtons[1], fieldButtons[2]);
         }
         else if(fieldButtons[0].text != string.Empty && fieldButtons[0].text.Equals(fieldButtons[3].text) && fieldButtons[0].text.Equals(fieldButtons[6].text))
         {
-            winner();
-            winnerButtons(fieldButtons[0], fieldButtons[4], fieldButtons[6]);
+            Winner();
+            WinnerButtons(fieldButtons[0], fieldButtons[4], fieldButtons[6]);
         }
         else if(fieldButtons[1].text != string.Empty && fieldButtons[1].text.Equals(fieldButtons[4].text) && fieldButtons[1].text.Equals(fieldButtons[7].text))
         {
-            winner();
-            winnerButtons(fieldButtons[1], fieldButtons[4], fieldButtons[7]);
+            Winner();
+            WinnerButtons(fieldButtons[1], fieldButtons[4], fieldButtons[7]);
         }
         else if(fieldButtons[2].text != string.Empty && fieldButtons[2].text.Equals(fieldButtons[5].text) && fieldButtons[2].text.Equals(fieldButtons[8].text))
         {
-            winner();
-            winnerButtons(fieldButtons[2], fieldButtons[5], fieldButtons[8]);
+            Winner();
+            WinnerButtons(fieldButtons[2], fieldButtons[5], fieldButtons[8]);
         }
         else if(clicks == 9)
         {
-            nowinner();
+            NoWinner();
         }
         else
             return;
 
     }
 
-    private void winnerButtons(TextMeshProUGUI textMeshProUGUI1, TextMeshProUGUI textMeshProUGUI2, TextMeshProUGUI textMeshProUGUI3)
+    private void WinnerButtons(TextMeshProUGUI textMeshProUGUI1, TextMeshProUGUI textMeshProUGUI2, TextMeshProUGUI textMeshProUGUI3)
     {
         textMeshProUGUI1.color = Color.red;
         textMeshProUGUI1.transform.DOPunchScale(new Vector3(0.5f, 0.5f, 0.5f), 1, 2, 1);
@@ -121,31 +118,25 @@ public class TicTacToeManager : MonoBehaviour
 
     }
 
-    private void winner()
+    private void Winner()
     {
         gameended = true;
         audioSource.PlayOneShot(winClip, 1f);
         if (currentPlayer == 1)
 
-            playertext.text = "Spieler X hat gewonnen";
+            playerText.text = "Spieler X hat gewonnen";
 
         else
-            playertext.text = "Spieler 0 hat gewonnen";
-        playertext.transform.DOShakeScale(5f,2,3);
-        playertext.transform.DOShakePosition(5, 2,3);
+            playerText.text = "Spieler 0 hat gewonnen";
+        playerText.transform.DOShakeScale(5f,2,3);
+        playerText.transform.DOShakePosition(5, 2,3);
     }
-    //private void winnerButtons(TextMeshProUGUI[] winningButtons)
-    //{
-    //    foreach (var button in winningButtons) 
-    //    {
-    //        button.color = Color.red;
-    //    }
-    //}
+ 
     
     
-    private void nowinner()
+    private void NoWinner()
     {
-        playertext.text = "Unentschieden";
+        playerText.text = "Unentschieden";
     }
 
     public void RestartGame()
